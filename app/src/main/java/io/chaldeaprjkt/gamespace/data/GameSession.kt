dfe9541083b28480e9a96d3a502dd902811dd40b
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2024-07-11 18:52:29 +0330

    GameSpace: Support Edge Cutout [4/4]
    Change-Id: I0501585ae17a273749f8842765eb85f54a479713
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@libremobileos.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/GameSession.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/GameSession.kt
@@ -59,6 +59,7 @@ class GameSession @Inject constructor(
             headsUp = systemSettings.headsUp,
             threeScreenshot = systemSettings.threeScreenshot,
             ringerMode = audioManager.ringerModeInternal,
+            edgecutout = systemSettings.edgecutout,
         )
         if (appSettings.noHeadsUp) {
             systemSettings.headsUp = false
@@ -68,6 +69,9 @@ class GameSession @Inject constructor(
         }
         if (appSettings.noThreeScreenshot) {
             systemSettings.threeScreenshot = false
+        }
+        if (appSettings.edgeCutout) {
+            systemSettings.edgecutout = true
         }
         audioManager.ringerModeInternal = appSettings.ringerMode
     }
@@ -82,6 +86,9 @@ class GameSession @Inject constructor(
         }
         if (appSettings.noThreeScreenshot) {
             orig.threeScreenshot?.let { systemSettings.threeScreenshot = it }
+        }
+        if (appSettings.edgeCutout) {
+            orig.edgecutout?.let { systemSettings.edgecutout = it }
         }
         audioManager.ringerModeInternal = orig.ringerMode
         state = null
